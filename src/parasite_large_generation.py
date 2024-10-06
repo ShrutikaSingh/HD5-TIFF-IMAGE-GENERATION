1379547700b3e33f1b2d7451c1cdb9ed13b18711
ShrutikaSingh <shrutika051220@gmail.com>
2024-10-06 00:33:04 -0700

    geneartion of large images working

--- src/parasite_large_generation.py
+++ src/parasite_large_generation.py
@@ -1,25 +1,30 @@
 import numpy as np
-from config import WIDTH, HEIGHT, HDF5_100K_DIR, DYED_REGIONS
+from config import WIDTH, HEIGHT, HDF5_100K_DIR
 from constants import WHITE, BLACK
-from messages import DYED_LARGE_IMAGE_MESSAGE, PARASITE_LARGE_IMAGE_MESSAGE
+from messages import PARASITE_LARGE_IMAGE_MESSAGE
 from utility import write_hdf5, timer_start, timer_end
-from numba import njit
+from numba import njit, prange
 from concurrent.futures import ThreadPoolExecutor
+
+@njit(parallel=True)
+def modify_image(image):
+    """ Modify the image with defined regions in parallel. """
+    image[10000:50000, 20000:50000] = WHITE
+    image[10000:30000, 50000:70000] = BLACK
+
+@njit(parallel=True)
+def modify_dyed_image(image):
+    """ Modify the dyed image with defined regions in parallel. """
+    image[10000:50000, 20000:50000] = 255
+    image[10000:30000, 50000:70000] = 0
 
 class ParasiteLargeImageGenerator:
     def __init__(self):
         self.image = np.zeros((HEIGHT, WIDTH), dtype=np.uint8)
 
-    @njit
-    def modify_image(self):
-        """ Modify the image with defined regions. """
-        # Example regions for modification
-        self.image[10000:50000, 20000:50000] = WHITE
-        self.image[10000:30000, 50000:70000] = BLACK
-
     def generate(self):
         start_time = timer_start()
-        self.modify_image()  # Call the optimized function to modify the image
+        modify_image(self.image)  # Pass the image to the Numba function
 
         # Write to HDF5 in a thread pool
         with ThreadPoolExecutor(max_workers=1) as executor:
@@ -27,25 +32,30 @@ class ParasiteLargeImageGenerator:
 
         timer_end(start_time, PARASITE_LARGE_IMAGE_MESSAGE)
 
-
 class ParasiteLargeDyedImageGenerator:
     def __init__(self):
         self.image = np.zeros((HEIGHT, WIDTH), dtype=np.uint8)
 
-    @njit
-    def modify_dyed_image(self):
-        """ Modify the dyed image with defined regions. """
-        for x_start, x_end, y_start, y_end, value in DYED_REGIONS:
-            self.image[x_start:x_end, y_start:y_end] = value
-
     def generate(self):
         start_time = timer_start()
-        self.modify_dyed_image()  # Call the optimized function to modify the image
+        modify_dyed_image(self.image)  # Pass the image to the Numba function
 
         # Write to HDF5 in a thread pool
         with ThreadPoolExecutor(max_workers=1) as executor:
             executor.submit(write_hdf5, f'{HDF5_100K_DIR}/dyed_large.h5', 'dyed_large', self.image, compression="gzip")
 
-        timer_end(start_time, DYED_LARGE_IMAGE_MESSAGE)
+        timer_end(start_time, "Dyed large image processing")
 
-# Example usage in main.py remains unchanged
+# Main function to generate images and test for cancer
+def main():
+    # Generate large parasite and dyed images
+    print("Generating large parasite images...")
+    large_parasite_generator = ParasiteLargeImageGenerator()
+    large_parasite_generator.generate()
+
+    print("Generating large dyed images...")
+    large_dyed_generator = ParasiteLargeDyedImageGenerator()
+    large_dyed_generator.generate()
+
+if __name__ == "__main__":
+    main()
